5167bab8bcf2cb5243892f7e580ff951c30b9d4e
MinJ00choi <111823130+MinJ00choi@users.noreply.github.com>
2023-01-04 17:06:31 +0900

    CMJ
    d

---
+++ Assets/Scripts/FunctionCS/DrawingSettings.cs
@@ -0,0 +1,111 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace FreeDraw
+{
+    // Helper methods used to set drawing settings
+    public class DrawingSettings : MonoBehaviour
+    {
+        public static bool isCursorOverUI = false;
+        public float Transparency = 1f;
+        Drawable test = null;
+
+        private void Start()
+        {
+            test = FindObjectOfType<Drawable>();
+        }
+
+
+        // Changing pen settings is easy as changing the static properties Drawable.Pen_Colour and Drawable.Pen_Width
+        public void SetMarkerColour(Color new_color)
+        {
+            Drawable.Pen_Colour = new_color;
+            test.AAAAA = new_color;
+        }
+        // new_width is radius in pixels
+        public void SetMarkerWidth(int new_width)
+        {
+            Drawable.Pen_Width = new_width;
+        }
+        public void SetMarkerWidth(float new_width)
+        {
+            SetMarkerWidth((int)new_width);
+        }
+
+        public void SetTransparency(float amount)
+        {
+            Transparency = amount;
+            Color c = Drawable.Pen_Colour;
+            c.a = amount;
+            Drawable.Pen_Colour = c;
+        }
+
+
+        // Call these these to change the pen settings
+        public void SetMarkerRed()
+        {
+            Color c = Color.red;
+            c.a = Transparency;
+            SetMarkerColour(c);
+            Drawable.drawable.SetPenBrush();
+        }
+        public void SetMarkerOrange()
+        {
+            SetMarkerColour(new Color(255f, 140f, 0f, 255f));
+        }
+        public void SetMarkerYellow()
+        {
+            Color c = Color.yellow;
+            c.a = Transparency;
+            SetMarkerColour(c);
+            Drawable.drawable.SetPenBrush();
+        }
+        public void SetMarkerGreen()
+        {
+            Color c = Color.green;
+            c.a = Transparency;
+            SetMarkerColour(c);
+            Drawable.drawable.SetPenBrush();
+        }
+        public void SetMarkerBlue()
+        {
+            Color c = Color.blue;
+            c.a = Transparency;
+            SetMarkerColour(c);
+            Drawable.drawable.SetPenBrush();
+        }
+        public void SetMarkerIndigo()
+        {
+            SetMarkerColour(new Color(75f, 0f, 130f, 255f));
+        }
+        public void SetMarkerPupple()
+        {
+            SetMarkerColour(new Color(128f, 0f, 128f, 255f));
+        }
+        public void SetMarkerBlack()
+        {
+            Color c = Color.black;
+            c.a = Transparency;
+            SetMarkerColour(c);
+            Drawable.drawable.SetPenBrush();
+        }
+        public void SetMarkerWhite()
+        {
+            Color c = Color.white;
+            c.a = Transparency;
+            SetMarkerColour(c);
+            Drawable.drawable.SetPenBrush();
+        }
+        public void SetEraser()
+        {
+            SetMarkerColour(new Color(255f, 255f, 255f, 0f));
+        }
+
+        public void PartialSetEraser()
+        {
+            SetMarkerColour(new Color(255f, 255f, 255f, 0.5f));
+        }
+    }
+}
