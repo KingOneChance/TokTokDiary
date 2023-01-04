532aa39b2c71a3b49ab49f8d8a12f3b9c2a173a3
KingOneChance <wonchan32@gmail.com>
2023-01-04 09:40:33 +0900

    test

---
+++ Assets/Scripts/Rules.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Our Programming Rules
+/// </summary>
+public class Rules 
+{
+    /// <summary>
+    /// 
+    /// 
+    /// 
+    /// 
+    /// 
+    /// 
+    /// 
+    /// 
+    /// 
+    /// 
+    /// 
+    /// 
+    /// 
+    /// 
+    /// 
+    /// 
+    /// 
+    /// 
+    /// 
+    /// 
+    /// 
+    /// 
+    /// </summary>
+}
