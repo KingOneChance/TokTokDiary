41e602b691097518572401b62e08f8b8df72bf47
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-01-16 21:22:03 +0900

    Add keystore to jenkins build

--- Assets/Editor/JenkinsBuild.cs
+++ Assets/Editor/JenkinsBuild.cs
@@ -5,6 +5,8 @@ public class JenkinsBuild
     [MenuItem("JenkinsBuild/Build")]
     static void JenkinsBuildRun()
     {
+        PlayerSettings.keyaliasPass = "qwerasdf";
+        PlayerSettings.keystorePass = "qwerasdf";
         //string[] scenes = { "Assets/Scenes/SampleScene.unity" };
         //string curDir = Directory.GetCurrentDirectory() + "\\Build\\"; //본인 프로젝트 경로에 Build란 폴더
         //string path = EditorUtility.SaveFolderPanel("Choose Location of Built Game", "", "");
