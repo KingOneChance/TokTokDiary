94103a3b19148a7e6b2be2ce4b156ea7b3c3dadd
KingOneChance <wonchan32@gmail.com>
2023-01-16 11:01:12 +0900

    BubbleBear
    Update

---
+++ Assets/Scripts/FunctionCS/Func_BubbleBearSign.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Func_BubbleBearSign : MonoBehaviour
+{
+    private Sprite saveSprite = null;
+    public void GetSaveSPrite(Sprite sprite)
+    {
+        saveSprite = sprite;
+    }
+}
