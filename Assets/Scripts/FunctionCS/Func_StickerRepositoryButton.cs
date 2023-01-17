2e650a7896dc58f71a632cdd957d0e3508e3c6c0
Dongfoo <chaezaroad@gmail.com>
2023-01-17 16:16:32 +0900

    dongfoo1616

--- Assets/Scripts/FunctionCS/Func_StickerRepositoryButton.cs
+++ Assets/Scripts/FunctionCS/Func_StickerRepositoryButton.cs
@@ -4,5 +4,9 @@ using UnityEngine;
 
 public class Func_StickerRepositoryButton : MonoBehaviour
 {
-    public void OnClick_ButtonOn() => Manager_Main.Instance.OnClick_StickerRepositoryOn();
+    public void OnClick_ButtonOn() 
+    {
+        Manager_Main.Instance.OnClick_StickerRepositoryOn();
+        Manager_Main.Instance.UI_StickerRepository.OnClick_RepositoryOpen();
+    }
 }
