ea75f90d47cf8103f130067ec7d4d79bf23b9684
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-01-18 11:48:32 +0900

    Update JenkinsBuild.cs
    By KJH

--- Assets/Editor/JenkinsBuild.cs
+++ Assets/Editor/JenkinsBuild.cs
@@ -24,6 +24,8 @@ public class JenkinsBuild
     [MenuItem("JenkinsBuild/Build")]
     public static void JenkinsBuildRun()
     {
+        PlayerSettings.keyaliasPass = "qwerasdf";
+        PlayerSettings.keystorePass = "qwerasdf";
         BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
         buildPlayerOptions.scenes = UnityEditor.EditorBuildSettingsScene.GetActiveSceneList(UnityEditor.EditorBuildSettings.scenes);
         buildPlayerOptions.locationPathName = string.Format("Build/≈Â≈Â¥Ÿ¿ÃæÓ∏Æ_{0}.apk", PlayerSettings.bundleVersion);
