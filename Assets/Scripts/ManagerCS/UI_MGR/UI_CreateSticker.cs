bc70596051e248ab84eed463d38289a92cec973b
KingOneChance <wonchan32@gmail.com>
2023-01-18 17:35:41 +0900

    StickerFactory
    update

--- Assets/Scripts/ManagerCS/UI_MGR/UI_CreateSticker.cs
+++ Assets/Scripts/ManagerCS/UI_MGR/UI_CreateSticker.cs
@@ -15,49 +15,4 @@ public class UI_CreateSticker : MonoBehaviour
     [Header("½ºÆ¼Ä¿")]
     [SerializeField] RawImage[] ui_Stickers = null;
     [SerializeField] Button[] ui_StickersBtns = null;
-
-    private Func_Camera func_Camera = null;
-
-    private void Start()
-    {
-        func_Camera = FindObjectOfType<Func_Camera>();
-        Manager_Main.Instance.UI_StickerRepository.OnClick_RepositoryOpen();
-    }
-    public void OnClick_WaffleRepository()
-    {
-        LoadLocalStickerInventory(Manager_Main.Instance.UI_StickerRepository.waffleList);
-    }
-    public void OnClick_AudioRepository()
-    {
-        LoadLocalStickerInventory(Manager_Main.Instance.UI_StickerRepository.audioList);
-    }
-    public void OnClick_WeatherRepository()
-    {
-        LoadLocalStickerInventory(Manager_Main.Instance.UI_StickerRepository.weatherList);
-    }
-    public void OnClick_DiaryRepository()
-    {
-        LoadLocalStickerInventory(Manager_Main.Instance.UI_StickerRepository.diaryList);
-    }
-    public void LoadLocalStickerInventory(List<string> anyList)
-    {
-        //initiate raw images's texture
-        for (int i = 0; i < ui_Stickers.Length; i++)
-        {
-            ui_Stickers[i].texture = null;
-        }
-        //Fill in the raw image's texture
-        for (int i = 0; i < anyList.Count; i++)
-        {
-            byte[] byteTexture = File.ReadAllBytes(anyList[i]);
-
-            if (byteTexture.Length > 0)
-            {
-                Texture2D texture = new Texture2D(0, 0);
-                texture.LoadImage(byteTexture);
-
-                ui_Stickers[i].texture = texture;
-            }
-        }   
-    }
 }
