5339f9f6e9fa814ec45ad5d3da328cba4ca84ea7
Dongfoo <chaezaroad@gmail.com>
2023-01-25 18:07:25 +0900

    dongfoo1800

--- Assets/Scripts/FunctionCS/Func_OnTriggerStay.cs
+++ Assets/Scripts/FunctionCS/Func_OnTriggerStay.cs
@@ -8,21 +8,40 @@ public class Func_OnTriggerStay : MonoBehaviour
     private float flowTime = 0;
     private Color mat;
 
+    [SerializeField] RectTransform[] positions = null;
+    [SerializeField] private Sprite image = null;
+
+    GameObject go = null;
+    Color hideImgColor = new Color(0, 0, 0, 0);
+    Color showImgColor = new Color(255, 255, 255, 255);
+    public int ranNum = 0;
     private void Start()
     {
-        mat = new Color(0, 0, 0,0);
+        ranNum = Random.Range(0, positions.Length);
+        go = new GameObject();
+        go.name = "HideSticker";
+        go.tag = "Find";
+
+        go.AddComponent<RawImage>().texture = image.texture;
+        go.GetComponent<RawImage>().color = hideImgColor;
+        go.transform.parent = positions[ranNum];
+        go.transform.position = positions[ranNum].transform.position;
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.tag == "Image")
+        if (collision.CompareTag("Image") || collision.CompareTag("Find"))
         {
-
             flowTime += Time.deltaTime;
             if (flowTime > 2.0f)
             {
+                go.GetComponent<RawImage>().color = showImgColor;
+                if(go.GetComponent<RawImage>().color == showImgColor)
+                {
+                    //이미지 인벤토리 추가
+                }
                 flowTime = 0;
                 Debug.Log("펑");
-                collision.GetComponent<Image>().color = mat;
+                collision.GetComponent<Image>().color = hideImgColor;
             }
         }
         else
@@ -30,6 +49,7 @@ public class Func_OnTriggerStay : MonoBehaviour
             flowTime = 0;
         }
     }
+ 
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.tag == "Image")
