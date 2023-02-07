e606e36acf45d950a5555d13462e396eae5ffdb5
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-02-07 17:38:56 +0900

    Commit Before merge from main

--- Assets/Scripts/ManagerCS/Manager_DiaryCase.cs
+++ Assets/Scripts/ManagerCS/Manager_DiaryCase.cs
@@ -1,8 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Profiling;
+using UnityEngine.UI;
 
 public class Manager_DiaryCase : MonoBehaviour
 {
+    [SerializeField] GameObject[] Profiles = null;
+    [SerializeField] RawImage[] ProfileImages = null;
+    [SerializeField] TextMeshProUGUI[] ProfileName = null;
 
+    private string path = "";
+    private void Start()
+    {
+        path = Application.persistentDataPath  + "/Profile";
+        DirectoryInfo di = new DirectoryInfo(path);
+
+        for(int i = 0; i < di.GetDirectories().Length; i++)
+        {
+            ProfileName[i].text = di.GetDirectories()[i].Name;
+        }
+
+        for (int i = 0; i < ProfileName.Length; i++)
+        {
+            if (ProfileName[i].text == "")
+            {
+                Profiles[i].GetComponent<Button>().interactable = true;
+                ProfileImages[i].color = Vector4.zero;
+            } 
+        }
+    }   
 }
