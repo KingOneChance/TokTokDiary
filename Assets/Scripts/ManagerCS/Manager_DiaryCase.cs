e5acf5bbbffcc9bdc9c5570fbd60287033f8482e
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-02-08 14:36:51 +0900

    Fix SwellUp Function

--- Assets/Scripts/ManagerCS/Manager_DiaryCase.cs
+++ Assets/Scripts/ManagerCS/Manager_DiaryCase.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,12 +12,15 @@ public class Manager_DiaryCase : MonoBehaviour
     [SerializeField] Button[] profileButton = null;
     [SerializeField] TextMeshProUGUI[] profileName = null;
 
+    [SerializeField] RawImage previewImg = null;
+
+    [SerializeField] List<Texture2D> diaryList = new List<Texture2D>();
     [SerializeField] private string selectedProfileName = "";
-    private string selectedProfilPath = "";
+    [SerializeField] private string selectedProfilPath = "";
 
     private void Start()
     {
-        for(int i = 0; i < profiles.Length; i++)
+        for (int i = 0; i < profiles.Length; i++)
         {
             if (profiles[i].texture == null) profileButton[i].interactable = false;
         }
@@ -23,13 +29,27 @@ public class Manager_DiaryCase : MonoBehaviour
     public void OnClick_Profile(int idx)
     {
         selectedProfileName = profileName[idx - 1].text;
-        selectedProfilPath = Application.persistentDataPath + "/" + selectedProfileName + "/";
+        selectedProfilPath = Application.persistentDataPath + "/Profile/" + selectedProfileName + "/" + "Diary";
+        CheckDiaryFiles();
         panels[0].SetActive(false);
         panels[1].SetActive(true);
     }
 
     private void CheckDiaryFiles()
     {
+        string[] allFiles = Directory.GetFiles(selectedProfilPath, "*.png", SearchOption.AllDirectories);
 
+        for (int i = 0; i < allFiles.Length; i++)
+        {
+            byte[] byteTexture = File.ReadAllBytes(allFiles[i]);
+
+            if (byteTexture.Length > 0)
+            {
+                Texture2D texture = new Texture2D(0, 0);
+                texture.LoadImage(byteTexture);
+                diaryList.Add(texture);
+            }
+        }
+        previewImg.texture = diaryList[0];
     }
 }
