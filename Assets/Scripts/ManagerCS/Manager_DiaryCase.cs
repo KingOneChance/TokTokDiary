525f46262e17bad3b1b679773e65ba71aea21f79
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-02-08 12:08:44 +0900

    commit before merge from main

--- Assets/Scripts/ManagerCS/Manager_DiaryCase.cs
+++ Assets/Scripts/ManagerCS/Manager_DiaryCase.cs
@@ -1,13 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.IO;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Profiling;
 using UnityEngine.UI;
 
 public class Manager_DiaryCase : MonoBehaviour
 {
-    
+    [SerializeField] RawImage[] profiles = null;
+    [SerializeField] GameObject[] panels = null;
+    [SerializeField] Button[] profileButton = null;
+    [SerializeField] TextMeshProUGUI[] profileName = null;
+
+    [SerializeField] private string selectedProfileName = "";
+    private string selectedProfilPath = "";
+
+    private void Start()
+    {
+        for(int i = 0; i < profiles.Length; i++)
+        {
+            if (profiles[i].texture == null) profileButton[i].interactable = false;
+        }
+    }
+
+    public void OnClick_Profile(int idx)
+    {
+        selectedProfileName = profileName[idx - 1].text;
+        selectedProfilPath = Application.persistentDataPath + "/" + selectedProfileName + "/";
+        panels[0].SetActive(false);
+        panels[1].SetActive(true);
+    }
+
+    private void CheckDiaryFiles()
+    {
+
+    }
 }
