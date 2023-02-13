7d7737b316f086d743691565d81eb5efc8aaf4af
MinJ00choi <111823130+MinJ00choi@users.noreply.github.com>
2023-02-13 12:18:25 +0900

    CMJ

---
+++ Assets/Scripts/FunctionCS/Func_Helper.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Func_Helper : MonoBehaviour
+{
+    [Header("도움말 선택 버튼")]
+    [SerializeField] Button Select_1;
+    [SerializeField] Button Select_2;
+    [SerializeField] Button Select_3;
+    [SerializeField] Button Select_4;
+
+    [SerializeField] Button Finish;
+    [SerializeField] RawImage translucent;
+    [SerializeField] Image example_StickerInventory;
+}
