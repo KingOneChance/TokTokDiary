151e35823161e796875781f421f6824bd6d09f60
MinJ00choi <111823130+MinJ00choi@users.noreply.github.com>
2023-02-16 17:00:00 +0900

    CMJ

--- Assets/Scripts/ManagerCS/Manager_FreeSticker.cs
+++ Assets/Scripts/ManagerCS/Manager_FreeSticker.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Manager_FreeSticker : Func_SaveSticker
 {
+    [SerializeField] public RawImage rawImage; // raw 이미지를 참조할 RawImage 컴포넌트
     // save point override;
     protected override void Start()
     {
@@ -17,10 +19,16 @@ public class Manager_FreeSticker : Func_SaveSticker
         widthValue = 1290;
         //(int)saveImageRect.rect.width;
         heightValue = 740;
-            //(int)saveImageRect.rect.height;
+        //(int)saveImageRect.rect.height;
     }
     public void OnClick_Save()
     {
         base.OnClick_SaveImgae(StickerType.FreeSticker);
     }
+    public void OnClick_ScaleControl(float scale) // 버튼 클릭 이벤트 핸들러
+    {
+        rawImage.transform.localScale = new Vector3(scale, scale, 1f); // raw 이미지의 스케일 값을 조절
+    }
 }
+
+
